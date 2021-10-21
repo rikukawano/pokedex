5ab6a2d7303482422de80dafe448d75d10dfc68f
Riku Kawano <riku.kawano@zmp.co.jp>
2021-10-21 09:30:28 +0900

    created pokemon api client

---
+++ lib/services/pokemon_api_client.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class PokemonApiClient {
+  final String _baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+  // GET request
+  Future<dynamic> get(String url) async {
+    try {
+      final response = await http.get(Uri.parse(_baseUrl + url));
+      return _parseResponse(response.statusCode, response.body);
+    } on SocketException {
+      throw Exception("No Internet Connection");
+    }
+  }
+
+  String _parseResponse(int httpStatus, String responseBody) {
+    switch (httpStatus) {
+      case 200:
+      case 201:
+        return responseBody;
+      case 400:
+        throw Exception('400 Bad Request');
+      case 401:
+        throw Exception('401 Unauthorized');
+      case 403:
+        throw Exception('403 Forbidden');
+      case 404:
+        throw Exception('404 Not Found');
+      case 405:
+        throw Exception('405 Method Not Allowed');
+      case 500:
+        throw Exception('500 Internal Server Error');
+      default:
+        throw Exception('Http status $httpStatus unknown error.');
+    }
+  }
+}
