40cf7e7d48615a24986e1137800349d60b1c7e81
Riku Kawano <riku.kawano@zmp.co.jp>
2021-10-25 15:53:05 +0900

    remove type

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -4,13 +4,11 @@ class Pokemon {
   final int id;
   final String name;
   final Sprites sprites;
-  final List<Types> types;
 
   Pokemon({
     required this.id,
     required this.name,
     required this.sprites,
-    required this.types,
   });
 
   factory Pokemon.fromMap(Map<String, dynamic> map) {
@@ -18,7 +16,6 @@ class Pokemon {
       id: map['id'].toInt(),
       name: map['name'],
       sprites: Sprites.fromMap(map['sprites']),
-      types: List<Types>.from(map['types']?.map((x) => Types.fromMap(x))),
     );
   }
 
@@ -41,34 +38,4 @@ class Sprites {
 
   factory Sprites.fromJson(String source) =>
       Sprites.fromMap(json.decode(source));
-}
-
-class Types {
-  final Type type;
-  Types({
-    required this.type,
-  });
-
-  factory Types.fromMap(Map<String, dynamic> map) {
-    return Types(
-      type: Type.fromMap(map['type']),
-    );
-  }
-
-  factory Types.fromJson(String source) => Types.fromMap(json.decode(source));
-}
-
-class Type {
-  final String name;
-  Type({
-    required this.name,
-  });
-
-  factory Type.fromMap(Map<String, dynamic> map) {
-    return Type(
-      name: map['name'],
-    );
-  }
-
-  factory Type.fromJson(String source) => Type.fromMap(json.decode(source));
 }
