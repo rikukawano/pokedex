19f1126ea7110d5ead78a991e42a0833ce85e132
Riku Kawano <riku.kawano@zmp.co.jp>
2021-11-08 20:55:01 +0900

    add pokemon stats

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -4,11 +4,13 @@ class Pokemon {
   final int id;
   final String name;
   final Sprites sprites;
+  final List<StatItem> stats;
 
   Pokemon({
     required this.id,
     required this.name,
     required this.sprites,
+    required this.stats,
   });
 
   factory Pokemon.fromMap(Map<String, dynamic> map) {
@@ -38,4 +40,44 @@ class Sprites {
 
   factory Sprites.fromJson(String source) =>
       Sprites.fromMap(json.decode(source));
+}
+
+class StatItem {
+  final int baseStat;
+  final int effort;
+  final Stat stat;
+  StatItem({
+    required this.baseStat,
+    required this.effort,
+    required this.stat,
+  });
+
+  factory StatItem.fromMap(Map<String, dynamic> map) {
+    return StatItem(
+      baseStat: map['base_stat']?.toInt(),
+      effort: map['effort']?.toInt(),
+      stat: Stat.fromMap(map['stat']),
+    );
+  }
+
+  factory StatItem.fromJson(String source) =>
+      StatItem.fromMap(json.decode(source));
+}
+
+class Stat {
+  final String name;
+  final String url;
+  Stat({
+    required this.name,
+    required this.url,
+  });
+
+  factory Stat.fromMap(Map<String, dynamic> map) {
+    return Stat(
+      name: map['name'],
+      url: map['url'],
+    );
+  }
+
+  factory Stat.fromJson(String source) => Stat.fromMap(json.decode(source));
 }
